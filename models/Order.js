ddb9126194a08c72421f7facad1de69a74c2aa4c
Tonalli <tonalli@xanum.uam.mx>
2020-03-28 10:54:35 -0600

    Add modes for product and order

--- models/Order.js
+++ models/Order.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const orderSchema = new Schema({
   products: [{type: Schema.Types.ObjectId, ref: 'Product'}],
-  owner: {type: Schema.Types.ObjectId, ref: 'User'}
+  customer: {type: Schema.Types.ObjectId, ref: 'User'}
 });
 
 const Order = mongoose.model('Order', orderSchema);
--- models/Product.js
+++ models/Product.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
-  name: {type: String, required: true},
-  price: {type: Number, min: 0, max: 1e4, required: true},
+  name: {type: String, trim: true, required: true},
+  price: {type: Number, default:0, min: 0, max: 1e4, required: true},
   available: {type: Number, min: 0, max:1e2, required: false},
   description: String,
-  imageURL: String,
+  imageURL: {type: String, trim: true, required: false},
 });
 
 const Product = mongoose.model('Product', productSchema);
