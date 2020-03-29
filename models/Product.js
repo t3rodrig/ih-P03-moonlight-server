118ebfa06a23d70dc8ccad790f4b8ad814598662
Tonalli <tonalli@xanum.uam.mx>
2020-03-29 10:20:18 -0600

    Update Product model

--- models/Product.js
+++ models/Product.js
@@ -4,8 +4,8 @@ const Schema = mongoose.Schema;
 const productSchema = new Schema({
   name: {type: String, trim: true, required: true},
   price: {type: Number, min: 0, max: 1e4, required: true},
-  available: {type: Number, default:0, min: 0, max:1e2, required: false},
-  description: String,
+  available: {type: Number, default:0, min: 0, max:1e2, required: true},
+  description: {type: String, required: false},
   imageURL: {type: String, trim: true, required: false},
 });
 
