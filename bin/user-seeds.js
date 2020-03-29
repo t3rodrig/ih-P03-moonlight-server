f88c4c99f123c98b107ec327ec60ed6c97d9c002
Tonalli <tonalli@xanum.uam.mx>
2020-03-29 13:22:58 -0600

    Update user model and user seed

--- bin/user-seeds.js
+++ bin/user-seeds.js
@@ -1,37 +1,56 @@
-// Seeds file that remove all users and create 2 new users
+// Seeds file that remove all users and create new users
 
 // To execute this seed, run from the root of the project
 // $ node bin/seeds.js
+require("dotenv").config();
 
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+
 const User = require("../models/User");
 
-const bcryptSalt = 10;
+const salt = bcrypt.genSaltSync(10);
+const hashPass = bcrypt.hashSync(process.env.DUMMY_PASS, salt);
 
 mongoose
-  .connect('mongodb://localhost/nightlight-server', {useNewUrlParser: true})
+  .connect(process.env.URL_DB, {useNewUrlParser: true})
   .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
   })
   .catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.error('Error connecting to mongo', err);
   });
 
 let users = [
   {
-    username: "alice",
-    password: bcrypt.hashSync("alice", bcrypt.genSaltSync(bcryptSalt)),
+    firstName: "Ana",
+    paternalLastName: "Martínez",
+    email: "ana@example.com",
+    password: hashPass
   },
   {
-    username: "bob",
-    password: bcrypt.hashSync("bob", bcrypt.genSaltSync(bcryptSalt)),
+    firstName: "Andrea",
+    paternalLastName: "López",
+    email: "andrea@example.com",
+    password: hashPass
+  },
+  {
+    firstName: "Alejandra",
+    paternalLastName: "Sánchez",
+    email: "alejandra@example.com",
+    password: hashPass
+  },
+  {
+    firstName: "Karla",
+    paternalLastName: "Flores",
+    email: "karla@example.com",
+    password: hashPass
   }
-]
+];
 
 User.deleteMany()
 .then(() => {
-  return User.create(users)
+  return User.create(users);
 })
 .then(usersCreated => {
   console.log(`${usersCreated.length} users created with the following id:`);
@@ -39,9 +58,9 @@ User.deleteMany()
 })
 .then(() => {
   // Close properly the connection to Mongoose
-  mongoose.disconnect()
+  mongoose.disconnect();
 })
 .catch(err => {
-  mongoose.disconnect()
-  throw err
-})
+  mongoose.disconnect();
+  throw err;
+});
--- models/User.js
+++ models/User.js
@@ -2,8 +2,23 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: String,
-  password: String
+  firstName: { type: String, required: true },
+  paternalLastName: { type: String, required: true },
+  maternalLastName: { type: String, required: false },
+  imageURL: { type: String, trim: true, required: false },
+  email: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  password: { type: String, required: true },
+  phone: { type: Number, required: false },
+  address: {
+    street: { type: String },
+    number: { type: Number },
+    borough: { type: String },
+    city: { type: String }
+  }
 }, {
   timestamps: {
     createdAt: 'created_at',
