ce0925a90ca91f8708eb2c7b68a620434f0b37db
Tonalli <tonalli@xanum.uam.mx>
2020-03-28 18:35:24 -0600

    Update Order model
    remove ref from products attribute

--- models/Order.js
+++ models/Order.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const orderSchema = new Schema({
-  products: [{type: Schema.Types.ObjectId, ref: 'Product'}],
+  products:{type: Array, required: true},
   customer: {type: Schema.Types.ObjectId, ref: 'User'}
 });
 
