8fabb43f24a758127f24a5202600ec8c53827fde
Tonalli Rodriguez <tonalli@xanum.uam.mx>
2020-04-13 17:18:58 -0500

    Update product-seeds.js

--- bin/product-seeds.js
+++ bin/product-seeds.js
@@ -7,52 +7,15 @@ const mongoose = require("mongoose");
 const Product = require("../models/Product");
 
 mongoose
-  .connect(process.env.URL_DB, {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err);
-  });
+.connect(process.env.URL_DB, {useNewUrlParser: true})
+.then(x => {
+  console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+})
+.catch(err => {
+  console.error('Error connecting to mongo', err);
+});
 
-let products = [
-  {
-    name: "product-1",
-    price: 10,
-    available: 10,
-    imageURL: "https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-product-1_medium.png"
-  },
-  {
-    name: "product-2",
-    price: 10,
-    available: 10,
-    imageURL: "https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-product-2_medium.png"
-  },
-  {
-    name: "product-3",
-    price: 10,
-    available: 10,
-    imageURL: "https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-product-3_medium.png"
-  },
-  {
-    name: "product-4",
-    price: 10,
-    available: 10,
-    imageURL: "https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-product-4_medium.png"
-  },
-  {
-    name: "product-5",
-    price: 10,
-    available: 10,
-    imageURL: "https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-product-5_medium.png"
-  },
-  {
-    name: "product-6",
-    price: 10,
-    available: 10,
-    imageURL: "https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-product-6_medium.png"
-  },
-];
+const products = require('./products.json');
 
 Product.deleteMany()
 .then(() => {
