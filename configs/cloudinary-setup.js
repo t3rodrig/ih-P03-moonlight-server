1cfdaa9ff6c82842663ee6c9ebb1b599320d9c67
Tonalli Rodriguez <tonalli@xanum.uam.mx>
2020-04-13 14:55:34 -0500

    Add cloudinary-setup

---
+++ configs/cloudinary-setup.js
@@ -0,0 +1,23 @@
+const multer = require("multer");
+const cloudinary = require("cloudinary");
+const cloudinaryStorage = require("multer-storage-cloudinary");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET
+});
+
+var storage = cloudinaryStorage({
+  cloudinary,
+  folder: "moonlight/products", // The name of the folder in cloudinary
+  allowedFormats: ["jpg", "png"],
+  // trasnformation: [{ width: 150, height: 150 }],
+  filename: function(req, file, cb) {
+    cb(null, file.originalname); // The file on cloudinary would have the same name as the original file name
+  }
+});
+
+const uploadCloud = multer({ storage });
+
+module.exports = uploadCloud;
